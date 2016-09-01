3b8e15ad7d0bd23d4472813b9ce08876cf68d2db
Zamboni Gianfranco <gzamboni@ws18.labo4>
2016-09-01 17:01:29 -0300

    Avanzando

--- atencional_cue_experiment.m
+++ atencional_cue_experiment.m
@@ -8,6 +8,8 @@ black=BlackIndex(wPtr);
 white = WhiteIndex(wPtr);
 gray = [128 128 128];
 
+imagen = imread('3.jpg','jpg');
+
 Screen('FillRect',wPtr,gray);  %Pintamos la pantalla de gris
 Screen('FillOval', wPtr, black, [640 512 660 532]); %Circulo de fijacion en el centro de la pantalla
 Screen(wPtr, 'Flip');
@@ -18,9 +20,14 @@ while toc<1 %segundos
 ;
 end
 
+cueCoords = [[320 256 350 286]; [960 256 990 286]; [635 507 665 537] ];   % Defino una matriz con las posiciones del cue atencional. Primer fila: Periferico izquierdo, Segunda fila: Periferico derecho, Tercer fila: Neutral
+fila = randi([1, 3], 1);  %Hacemos random entre 1 y 3 para decidir que cue atencional usar.
+
+faceCoords = [];
+
 Screen('FillRect',wPtr,gray);  %Pintamos la pantalla de gris
 Screen('FillOval', wPtr, black, [640 512 660 532]); %Circulo de fijacion en el centro de la pantalla
-Screen('FillOval', wPtr, black, [320 256 350 286]); %Cue perfiferico a la izquierda
+Screen('FillOval', wPtr, black, cueCoords(fila, :)); %Cue perfiferico a la izquierda; Usamos las coordenadas indicadas por fila (el numero random).
 Screen(wPtr, 'Flip');
 
 tic
@@ -28,25 +35,19 @@ while toc<1
 ;
 end
 
-Screen('FillRect',wPtr,gray);  %Pintamos la pantalla de gris
+
+Screen('PutImage', wPtr, imagen, [220 412 420 656]);
+Screen('PutImage', wPtr, imagen, [860 412 1060 656]);
 Screen('FillOval', wPtr, black, [640 512 660 532]); %Circulo de fijacion en el centro de la pantalla
-Screen('FillOval', wPtr, black, [960 256 990 286]); %Cue periferico a la derecha
-Screen(wPtr, 'Flip');
+Screen(wPtr,'Flip');
+
 
 tic
 while toc<1
 ;
 end
 
-Screen('FillRect',wPtr,gray);  %Pintamos la pantalla de gris
-Screen('FillOval', wPtr, black, [640 512 660 532]); %Circulo de fijacion en el centro de la pantalla
-Screen('FillOval', wPtr, black, [635 507 665 537]); %Cue periferico a la derecha
-Screen(wPtr, 'Flip');
 
-tic
-while toc<1
-;
-end
+while ~KbCheck; end % wait for a key press
 
-Screen('CloseAll');
-ShowCursor;
+Screen(wPtr,'Flip');
