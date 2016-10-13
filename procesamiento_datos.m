bea8e332437ee87296da95a4b96aed1b9fefa4d6
Gianfranco <gzamboni@dc.uba.ar>
2016-10-13 04:29:52 -0300

    Update procesamiento_datos.m

--- procesamiento_datos.m
+++ procesamiento_datos.m
@@ -4,7 +4,7 @@ for k = 1:cantFiles
     load(cat(2, 'Data/',dataFiles(k,:)));
     dataSize = size(fullData);
     heigth = dataSize(1);
-    subjectNumber = ones(heigth, 1)*k - 20;
+    subjectNumber = ones(heigth, 1)*k;
     fullData = cat(2, subjectNumber, fullData);
     if(k == 1) 
        allData = fullData([20:end],:); 
